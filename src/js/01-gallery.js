16923a830b5711a8ee4227cb3805ceca08d7432e
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-08-07 20:02:32 +0300

    hw 8 task 1 comm 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+
+const galleryItemsRef = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems.map(
+        ({ preview, original, description }) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}" />
+                </a>
+            </li>`
+        ).join('');
+    
+galleryItemsRef.innerHTML = createGalleryMarkup;
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
