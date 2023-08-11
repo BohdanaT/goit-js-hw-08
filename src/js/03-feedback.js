1232d194759c4624c803211ee884e83383dc64db
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-08-11 17:19:08 +0300

    hw 8 task 3 comm 1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,9 +4,8 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 
-
 form.addEventListener('input', onFormInput);
 
 function onFormInput(e) {
-    console.log(e.CurrentTarget);
+    console.log(e.target);
 }
