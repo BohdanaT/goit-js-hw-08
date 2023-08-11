3d2fc4dc54c9e7fa5b981da09d8d537556b0c54d
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-08-11 14:19:42 +0000

    Deploying to gh-pages from @ BohdanaT/goit-js-hw-08@1232d194759c4624c803211ee884e83383dc64db 🚀

--- 03-feedback.8a12316d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");document.querySelector('input[name="email"]'),document.querySelector('textarea[name="message"]');e.addEventListener("input",(function(e){console.log(e.CurrentTarget)}));
-//# sourceMappingURL=03-feedback.8a12316d.js.map
--- 03-feedback.babec15a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");document.querySelector('input[name="email"]'),document.querySelector('textarea[name="message"]');e.addEventListener("input",(function(e){console.log(e.CurrentTarget)}))}();
-//# sourceMappingURL=03-feedback.babec15a.js.map
