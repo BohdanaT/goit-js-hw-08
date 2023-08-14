476af36d9862f499f6415d225de90f6926eae46e
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-08-14 14:28:35 +0300

    hw 8 task 3 comm 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,49 @@
-
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
+const formData = {};
 
-form.addEventListener('input', onFormInput);
+populateFormData();
+
+// submit
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    if (form.email.value === '' || form.message.value === '') {
+        return alert('Please, fill in all the fields!');
+    };
+
+    const userData = {
+        email: form.email.value,
+        message: form.message.value,
+    };
+    console.log(userData);
+
+    localStorage.removeItem('feedback-form-state');
+    e.currentTarget.reset();
+}
+
+// saving data
+form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(e) {
-    console.log(e.target);
-}
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem('feedback-form-state',
+    JSON.stringify(formData));
+};
+
+// populating saved data
+function populateFormData() {
+    const savedFormData = localStorage.getItem('feedback-form-state');
+    const parsedFormData = JSON.parse(savedFormData);
+    
+    if (parsedFormData) {
+        form.email.value = parsedFormData.email || '';
+        form.message.value = parsedFormData.message || '';
+    }; 
+};
+
+
